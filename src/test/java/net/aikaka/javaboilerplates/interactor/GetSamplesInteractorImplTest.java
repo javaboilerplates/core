4b9c6a41f503c50c443f2930d61ad40145c7eb3e
Ai Nguyen <ainguyenkaka@gmail.com>
2018-05-01 22:02:59 +0800

    update tests

--- src/test/java/net/aikaka/javaboilerplates/interactor/GetSamplesInteractorImplTest.java
+++ src/test/java/net/aikaka/javaboilerplates/interactor/GetSamplesInteractorImplTest.java
@@ -20,7 +20,6 @@ public class GetSamplesInteractorImplTest {
 
     @Test
     public void testGetSamples() {
-
         List<SampleEntity> list = interactor.getSamples();
         assertNotNull(list);
         assertTrue(list.size() > 0);
@@ -35,9 +34,27 @@ public class GetSamplesInteractorImplTest {
     }
 
     @Test
-    public  void testConstructor() {
+    public void testConstructor() {
         interactor = new GetSamplesInteractorImpl(new ArrayList<>());
         List<SampleEntity> list = interactor.getSamples();
+        assertNotNull(list);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void testAll() {
+        List<SampleEntity> list = interactor.all();
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+    }
+
+    @Test
+    public void testAllByName() {
+        List<SampleEntity> list = interactor.allByName("ka");
+        assertNotNull(list);
+        assertTrue(list.size() > 0);
+
+        list = interactor.allByName(null);
         assertNotNull(list);
         assertEquals(0, list.size());
     }
